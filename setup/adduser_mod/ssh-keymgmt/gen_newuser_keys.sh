4a4908424586089fc46acd51a89ee40eb6cf26e5
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-07-11 11:50:26 +0530

    adduser_mod: Added ssh-keys handling - Takes user keys as input - Generates a new pair of keys for inter-node access

---
+++ setup/adduser_mod/ssh-keymgmt/gen_newuser_keys.sh
@@ -0,0 +1,31 @@
+# Script to :
+#	1. Get user input for custom keys and add them to authorized_keys
+#	2. Generate new set of keys for inter-node access in the cluster
+# Author: Srikar (aka epoch101)
+
+# $1 - username
+# $2 - UID
+# $3 - GI
+# SSH_DIR - /home/$1/.ssh
+
+SSH_DIR = /home/$1/.ssh
+
+# Making a new ssh directory
+mkdir $SSH_DIR
+chmod 700 $SSH_DIR
+
+# Generating new keys
+ssh-keygen -t ed25519 -f $SSH_DIR/id_ed25519 -q -N ""
+
+# Adding the public key of new keys for inter-node access
+cat $SSH_DIR/id_ed25519.pub > $SSH_DIR/authorized_keys
+
+# Getting SSH keys from user
+echo -n "Enter user's SSH keys: "
+read user_sshkeys
+
+# Adding user keys to username for easy access
+echo user_sshkeys >> $SSH_DIR/authorized_keys
+
+# Changing file permissions to ensure proper access
+chown -R $2:$3 $SSH_DIR
