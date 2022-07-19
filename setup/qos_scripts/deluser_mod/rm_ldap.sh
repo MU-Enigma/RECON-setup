5eb7427be8b0be7d28644cfd41bb0affba342101
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-07-20 00:04:10 +0530

    deluser_mod: LDAP entry deletion

---
+++ setup/qos_scripts/deluser_mod/rm_ldap.sh
@@ -0,0 +1,17 @@
+# File to be placed in /usr/local/sbin/deluser_mod/
+# Script to remove all LDAP entried of a user
+# Author: Srikar (aka epoch101)
+
+# $1 - username
+# $2 - UID
+# $3 - GID
+
+# Delete the user entry from LDAP
+echo "Removing LDAP entry: uid=$1,ou=users,dc=senpai"
+ldapdelete -x -D cn=admin,dc=senpai -w $(cat /etc/pam_ldap.conf) uid=$1,ou=users,dc=senpai
+
+# Delete the group entry from LDAP
+echo "Removing LDAP entry: cn=$1,ou=groups,dc=senpai"
+ldapdelete -x -D cn=admin,dc=senpai -w $(cat /etc/pam_ldap.conf) cn=$1,ou=groups,dc=senpai
+
+# TODO: Delete other group entries as well
